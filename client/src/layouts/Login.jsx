471431d6a8f698d68e32d291989107720f8de8db
arun-121 <arunramesh900@gmail.com>
2023-08-12 15:16:15 +0530

    Added animation in login page

--- client/src/layouts/Login.jsx
+++ client/src/layouts/Login.jsx
@@ -1,5 +1,10 @@
 "use client";
 
+import star1 from "../assets/star1.png";
+import star2 from "../assets/star2.png";
+import musicnotes from "../assets/music-note1.png";
+import musicnotes1 from "../assets/music-note1.png";
+
 import {
   Flex,
   Box,
@@ -15,7 +20,118 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 
+import Lottie from "lottie-react";
+import {
+  MouseParallaxContainer,
+  MouseParallaxChild,
+} from "react-parallax-mouse";
+import bg1 from "../assets/1.json";
+
 export default function Login() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <MouseParallaxContainer
+        containerStyle={{
+          display: "flex",
+          flexDirection: "row",
+          flex: "1",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <div style={{ flex: "1", display: "flex", flexDirection: "column" }}>
+            <div
+              style={{
+                position: "relative",
+                marginLeft: "20%",
+                marginTop: "100px",
+                flex: "1",
+              }}
+            >
+              <MouseParallaxChild
+                factorX={0.1}
+                factorY={0.1}
+                style={{
+                  position: "absolute",
+                  top: "100px",
+                  left: 0,
+                  width: "10%",
+                  height: "10%",
+                }}
+              >
+                <img src={star1} alt="" />
+              </MouseParallaxChild>
+              <MouseParallaxChild
+                factorX={0.2}
+                factorY={0.2}
+                style={{
+                  position: "absolute",
+                  top: "0",
+                  left: "100px",
+                  width: "10%",
+                  height: "10%",
+                }}
+              >
+                <img src={musicnotes1} alt="" />
+              </MouseParallaxChild>
+              <MouseParallaxChild
+                factorX={0.1}
+                factorY={0.1}
+                style={{
+                  position: "absolute",
+                  top: "100px",
+                  left: "20%",
+                  width: "10%",
+                  height: "10%",
+                }}
+              >
+                <img src={star2} alt="" />
+              </MouseParallaxChild>
+              <MouseParallaxChild
+                factorX={0.2}
+                factorY={0.2}
+                style={{
+                  position: "absolute",
+                  top: "0",
+                  left: "30%",
+                  width: "10%",
+                  height: "10%",
+                }}
+              >
+                <img src={musicnotes} alt="" />
+              </MouseParallaxChild>
+            </div>
+            <Lottie animationData={bg1} style={{ width: "480px", flex: "1" }} />
+          </div>
+          <div
+            style={{
+              height: "100%",
+              background: "#19282b",
+              flex: "1",
+            }}
+          >
+            <LoginForm />
+          </div>
+        </div>
+      </MouseParallaxContainer>
+    </div>
+  );
+}
+
+function LoginForm() {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
   const [status, setStatus] = useState("");
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Login from "./layouts/Login.jsx";
-import {ChakraProvider} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import AppTheme from "./theme.jsx";
 import Signup from "./layouts/Signup.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <ChakraProvider theme={AppTheme}>
-            <App/>
-            {/*<Login></Login>
-            <Signup></Signup>*/}
-        </ChakraProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={AppTheme}>
+      {/* <App/> */}
+      <Login></Login>
+      {/* <Signup></Signup> */}
+    </ChakraProvider>
+  </React.StrictMode>
 );
